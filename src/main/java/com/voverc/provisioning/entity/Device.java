d8b797415537ac475267d5f72ee90abe407bf0c2
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-03-28 22:12:57 +0200

    move logic to design driven and parse override fragments at device class

--- src/main/java/com/voverc/provisioning/entity/Device.java
+++ src/main/java/com/voverc/provisioning/entity/Device.java
@@ -1,15 +1,26 @@
 package com.voverc.provisioning.entity;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.voverc.provisioning.dto.FragmentDTO;
+import com.voverc.provisioning.utils.ParserUtils;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
 import javax.persistence.Id;
+import java.util.Arrays;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static com.voverc.provisioning.dto.OverrideFragment.DOMAIN;
+import static com.voverc.provisioning.dto.OverrideFragment.PORT;
+import static com.voverc.provisioning.dto.OverrideFragment.TIMEOUT;
 
 @Data
 @Entity
@@ -36,5 +47,40 @@ public class Device {
     public enum DeviceModel {
         CONFERENCE,
         DESK
+    }
+
+    public Optional<FragmentDTO> parseOverrideFragment() {
+        if (overrideFragment == null || overrideFragment.isEmpty())
+            return Optional.empty();
+
+        return ParserUtils.isPropertyFormat(overrideFragment) ?
+                parsePropertiesFragment() :
+                parseJsonFragment();
+    }
+
+    @SneakyThrows
+    private Optional<FragmentDTO> parseJsonFragment() {
+        ObjectMapper mapper = new ObjectMapper();
+        return Optional.of(mapper.readValue(overrideFragment, FragmentDTO.class));
+    }
+
+    private Optional<FragmentDTO> parsePropertiesFragment() {
+        FragmentDTO fragmentDTO = new FragmentDTO();
+        Stream<String> lines = Arrays.stream(overrideFragment.split("\n"));
+
+        lines.forEach(line -> {
+            if (line.contains(DOMAIN.getLowerCaseName())) {
+                fragmentDTO.setDomain(ParserUtils.getPropertyValue(line));
+
+            } else if (line.contains(PORT.getLowerCaseName())) {
+                String port = ParserUtils.getPropertyValue(line);
+                fragmentDTO.setPort(Integer.parseInt(port));
+
+            } else if (line.contains(TIMEOUT.getLowerCaseName())) {
+                String timeout = ParserUtils.getPropertyValue(line);
+                fragmentDTO.setTimeout(Integer.parseInt(timeout));
+            }
+        });
+        return Optional.of(fragmentDTO);
     }
 }
--- src/main/java/com/voverc/provisioning/service/ProvisioningServiceImpl.java
+++ src/main/java/com/voverc/provisioning/service/ProvisioningServiceImpl.java
@@ -1,26 +1,15 @@
 package com.voverc.provisioning.service;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.voverc.provisioning.config.ProvisioningProperties;
-import com.voverc.provisioning.dto.FragmentDTO;
 import com.voverc.provisioning.entity.ConfigurationFileResponse;
 import com.voverc.provisioning.entity.Device;
 import com.voverc.provisioning.exception.NotPresentedInDbException;
 import com.voverc.provisioning.repository.DeviceRepository;
-import com.voverc.provisioning.utils.ParserUtils;
 import lombok.AllArgsConstructor;
-import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.Validate;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
-import java.util.stream.Stream;
-
-import static com.voverc.provisioning.dto.OverrideFragment.DOMAIN;
-import static com.voverc.provisioning.dto.OverrideFragment.PORT;
-import static com.voverc.provisioning.dto.OverrideFragment.TIMEOUT;
 
 
 @Slf4j
@@ -44,47 +33,9 @@ public class ProvisioningServiceImpl implements ProvisioningService {
         BeanUtils.copyProperties(provisioningProperties, fileResponse);
 
         // additional provisioning flow
-        if (device.getOverrideFragment() != null) {
-
-            String fragment = device.getOverrideFragment();
-            FragmentDTO dto = (ParserUtils.isPropertyFormat(fragment)) ?
-                    parsePropertiesFragment(fragment) :
-                    parseJsonFragment(fragment);
-
-            BeanUtils.copyProperties(dto, fileResponse);
-        }
+        device.parseOverrideFragment()
+                .ifPresent(dto -> BeanUtils.copyProperties(dto, fileResponse));
 
         return fileResponse;
-    }
-
-
-    @SneakyThrows
-    private FragmentDTO parseJsonFragment(String fragment) {
-        Validate.notNull(fragment, "fragment can not be null");
-
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValue(fragment, FragmentDTO.class);
-    }
-
-    private FragmentDTO parsePropertiesFragment(String fragment) {
-        Validate.notNull(fragment, "fragment can not be null");
-
-        FragmentDTO fragmentDTO = new FragmentDTO();
-        Stream<String> lines = Arrays.stream(fragment.split("\n"));
-
-        lines.forEach(line -> {
-            if (line.contains(DOMAIN.getLowerCaseName())) {
-                fragmentDTO.setDomain(ParserUtils.getPropertyValue(line));
-
-            } else if (line.contains(PORT.getLowerCaseName())) {
-                String port = ParserUtils.getPropertyValue(line);
-                fragmentDTO.setPort(Integer.parseInt(port));
-
-            } else if (line.contains(TIMEOUT.getLowerCaseName())) {
-                String timeout = ParserUtils.getPropertyValue(line);
-                fragmentDTO.setTimeout(Integer.parseInt(timeout));
-            }
-        });
-        return fragmentDTO;
     }
 }
