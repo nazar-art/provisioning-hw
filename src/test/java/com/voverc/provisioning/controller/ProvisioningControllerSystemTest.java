7995d5a625f82ae3c96b81cd23664b8413f8fb85
Nazar Lelyak <nazar.lelyak@gmail.com>
2020-03-30 18:41:38 +0300

    use MediaType constant instead of string value

--- src/test/java/com/voverc/provisioning/controller/ProvisioningControllerSystemTest.java
+++ src/test/java/com/voverc/provisioning/controller/ProvisioningControllerSystemTest.java
@@ -5,7 +5,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
@@ -14,6 +13,7 @@ import org.springframework.web.context.WebApplicationContext;
 
 import static org.hamcrest.CoreMatchers.allOf;
 import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -39,9 +39,9 @@ public class ProvisioningControllerSystemTest {
     @Test
     public void configurationFileAreGeneratedWithTestProfilesData() throws Exception {
         this.mockMvc.perform(get("/api/v1/provisioning/aa-bb-cc-dd-ee-ff")
-                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(content().string(allOf(
                         containsString("{\"username\":\"john\",\"password\":\"doe\",\"domain\":\"sip.test.com\",\"port\":6090,\"codecs\":[\"G911\",\"G922\"],\"timeout\":0}")
                 )));
@@ -50,9 +50,9 @@ public class ProvisioningControllerSystemTest {
     @Test
     public void configurationFileAreAvailableWithPropertiesOverrideFragment() throws Exception {
         this.mockMvc.perform(get("/api/v1/provisioning/a1-b2-c3-d4-e5-f6")
-                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(content().string(allOf(
                         containsString("{\"username\":\"walter\",\"password\":\"white\",\"domain\":\"sip.anotherdomain.com\",\"port\":5161,\"codecs\":[\"G911\",\"G922\"],\"timeout\":10}")
                 )));
@@ -61,9 +61,9 @@ public class ProvisioningControllerSystemTest {
     @Test
     public void configurationFileAreAvailableWithJsonOverrideFragment() throws Exception {
         this.mockMvc.perform(get("/api/v1/provisioning/1a-2b-3c-4d-5e-6f")
-                .accept(MediaType.parseMediaType("application/json;charset=UTF-8")))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
+                .andExpect(content().contentType(APPLICATION_JSON))
                 .andExpect(content().string(allOf(
                         containsString("{\"username\":\"eric\",\"password\":\"blue\",\"domain\":\"sip.anotherdomain.com\",\"port\":5161,\"codecs\":[\"G911\",\"G922\"],\"timeout\":10}")
                 )));
@@ -72,7 +72,7 @@ public class ProvisioningControllerSystemTest {
     @Test
     public void ifMacAddressIsWrongThrowException() throws Exception {
         mockMvc.perform(get("/api/v1/provisioning/wrong-mac-address")
-                .accept(MediaType.APPLICATION_JSON))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isBadRequest());
     }
 }
--- src/test/java/com/voverc/provisioning/controller/ProvisioningRestControllerTest.java
+++ src/test/java/com/voverc/provisioning/controller/ProvisioningRestControllerTest.java
@@ -7,7 +7,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -15,6 +14,7 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import static org.hamcrest.Matchers.hasItem;
 import static org.hamcrest.Matchers.hasSize;
 import static org.mockito.Mockito.when;
+import static org.springframework.http.MediaType.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -41,7 +41,7 @@ public class ProvisioningRestControllerTest {
 
     @Test
     public void configurationFileIsReturnedForMacAddress() throws Exception {
-        ConfigurationFileResponse response = ConfigurationFileResponse.builder()
+        ConfigurationFileResponse expectedResponse = ConfigurationFileResponse.builder()
                 .username("A")
                 .password("password")
                 .domain("test.domain.com")
@@ -51,27 +51,27 @@ public class ProvisioningRestControllerTest {
                 .timeout(5)
                 .build();
 
-        when(provisioningService.getProvisioningFile("aa-bb-cc")).thenReturn(response);
+        when(provisioningService.getProvisioningFile("aa-bb-cc")).thenReturn(expectedResponse);
 
         mockMvc.perform(get("/api/v1/provisioning/aa-bb-cc")
-                .accept(MediaType.APPLICATION_JSON))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().contentType("application/json"))
-                .andExpect(jsonPath("$.username").value("A"))
-                .andExpect(jsonPath("$.password").value("password"))
-                .andExpect(jsonPath("$.domain").value("test.domain.com"))
-                .andExpect(jsonPath("$.port").value("1715"))
+                .andExpect(content().contentType(APPLICATION_JSON))
+                .andExpect(jsonPath("$.username").value(expectedResponse.getUsername()))
+                .andExpect(jsonPath("$.password").value(expectedResponse.getPassword()))
+                .andExpect(jsonPath("$.domain").value(expectedResponse.getDomain()))
+                .andExpect(jsonPath("$.port").value(expectedResponse.getPort()))
                 .andExpect(jsonPath("$.codecs").isArray())
                 .andExpect(jsonPath("$.codecs", hasSize(2)))
-                .andExpect(jsonPath("$.codecs", hasItem("SO")))
-                .andExpect(jsonPath("$.codecs", hasItem("GB")))
-                .andExpect(jsonPath("$.timeout").value("5"));
+                .andExpect(jsonPath("$.codecs", hasItem(expectedResponse.getCodecs().get(0))))
+                .andExpect(jsonPath("$.codecs", hasItem(expectedResponse.getCodecs().get(1))))
+                .andExpect(jsonPath("$.timeout").value(expectedResponse.getTimeout()));
     }
 
     @Test
     public void ifMacAddressIsEmptyThrowException() throws Exception {
         mockMvc.perform(get("/api/v1/provisioning/")
-                .accept(MediaType.APPLICATION_JSON))
+                .accept(APPLICATION_JSON))
                 .andExpect(status().isNotFound());
     }
 }
